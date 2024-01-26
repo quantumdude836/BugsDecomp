28da1ee18da2c707e35be49b364a1f62a723a4f2
Drew McGowen <quantumdude836@gmail.com>
2024-01-26 13:32:31 -0600

    Add level logic debug enhancement
    All enhancements will be disabled by default.

---
+++ BugsDecomp/enhancements.h
@@ -0,0 +1,8 @@
+
+// Configuration for enhancements to decompiled code.
+
+#pragma once
+
+
+// whether to enable debug logging for level logic
+#define ENH_DEBUG_LOGIC 0
--- BugsDecomp/logic.c
+++ BugsDecomp/logic.c
@@ -5,13 +5,22 @@
 #include "logic_prv.h"
 #include "misc.h"
 #include "pad.h"
+#include "enhancements.h"
+#include "dbg.h"
+
+
+// number of defined condition codes
+#define LOGIC_COND_COUNT 99
+
+// number of defined action codes
+#define LOGIC_ACTION_COUNT 129
 
 
 // condition handler table
-#define condTable (*(LOGIC_COND_FN (*)[99])0x4ac550)
+#define condTable (*(LOGIC_COND_FN (*)[LOGIC_COND_COUNT])0x4ac550)
 
 // action handler table
-#define actionTable (*(LOGIC_ACTION_FN (*)[129])0x4ac6e0)
+#define actionTable (*(LOGIC_ACTION_FN (*)[LOGIC_ACTION_COUNT])0x4ac6e0)
 
 // 256-byte scratch space
 #define logicScratch (*(BYTE (*)[256])0x4b2260)
@@ -20,6 +29,127 @@
 // "shortcuts" for handlers
 #define SCRATCH (logicScratch)
 #define GLOBALS (gameData->logicGlobals)
+
+
+#if ENH_DEBUG_LOGIC
+
+// helpers for converting condition/action codes to strings
+
+static const char *const condNameTable[LOGIC_COND_COUNT] = {
+    [0x00] = "true",
+    [0x01] = "ciseq",
+    [0x02] = "cigeq",
+    [0x03] = "cisltu",
+    [0x04] = "cigltu",
+    [0x05] = "cisleu",
+    [0x06] = "cigleu",
+    [0x07] = "cisgtu",
+    [0x08] = "ciggtu",
+    [0x09] = "cisgeu",
+    [0x0a] = "ciggeu",
+    [0x0f] = "csseq",
+    [0x10] = "cggeq",
+    [0x1e] = "tismnz",
+    [0x1f] = "tigmnz",
+    [0x22] = "tismz",
+    [0x23] = "tigmz",
+    [0x25] = "cisne",
+    [0x26] = "cigne",
+    [0x27] = "tisaz",
+    [0x28] = "tigaz",
+    [0x2e] = "tisxz",
+    [0x2f] = "tigxz",
+    [0x31] = "tbdact",
+    [0x33] = "tpnmv",
+    [0x35] = "tpmov",
+    [0x3d] = "cseq",
+    [0x3e] = "cgeq",
+    [0x3f] = "csne",
+    [0x40] = "cgne",
+    [0x41] = "csltu",
+    [0x42] = "cgltu",
+    [0x43] = "csgtu",
+    [0x44] = "cggtu",
+    [0x46] = "cislt",
+    [0x47] = "cisgt",
+    [0x48] = "cslt",
+    [0x49] = "csgt",
+    [0x51] = "tbuact",
+    [0x58] = "ciglt",
+    [0x59] = "ciggt",
+    [0x60] = "tkdesc",
+};
+
+static inline const char *CondName(BYTE code)
+{
+    static char buf[8];
+    const char *name = NULL;
+
+    // if code is valid, see if there's a known name
+    if (code < LOGIC_COND_COUNT)
+        name = condNameTable[code];
+
+    // generate default name if needed
+    if (!name)
+    {
+        sprintf_s(buf, sizeof buf, "cnd_%02x", code);
+        name = buf;
+    }
+
+    return name;
+}
+
+static const char *const actionNameTable[LOGIC_ACTION_COUNT] = {
+    [0x00] = "nop",
+    [0x03] = "incs",
+    [0x04] = "incg",
+    [0x05] = "decs",
+    [0x06] = "decg",
+    [0x07] = "clrs",
+    [0x08] = "clrg",
+    [0x09] = "negs",
+    [0x0a] = "negg",
+    [0x0b] = "orsi",
+    [0x0c] = "orgi",
+    [0x0d] = "andsi",
+    [0x0e] = "andgi",
+    [0x0f] = "rnds",
+    [0x10] = "rndg",
+    [0x12] = "movsi",
+    [0x13] = "movgi",
+    [0x14] = "movss",
+    [0x15] = "movgg",
+    [0x16] = "incss",
+    [0x17] = "incgg",
+    [0x1b] = "movgs",
+    [0x1c] = "movsg",
+    [0x24] = "addsi",
+    [0x25] = "addgi",
+    [0x27] = "subsi",
+    [0x28] = "subgi",
+    [0x30] = "gvcrt",
+};
+
+static inline const char *ActionName(BYTE code)
+{
+    static char buf[8];
+    const char *name = NULL;
+
+    // if code is valid, see if there's a known name
+    if (code < LOGIC_ACTION_COUNT)
+        name = actionNameTable[code];
+
+    // generate default name if needed
+    if (!name)
+    {
+        sprintf_s(buf, sizeof buf, "act_%02x", code);
+        name = buf;
+    }
+
+    return name;
+}
+
+#endif
 
 
 // helper for dealing with 2-byte golden carrot count
@@ -47,9 +177,23 @@ static inline void AddGlobal(WORD offset, WORD adj)
 
 BOOL TestLogicCond(void *ent, const LOGIC_ITEM *item)
 {
-    if (item->condCode < ARRAYSIZE(condTable))
-        return condTable[item->condCode](ent, item);
-    return FALSE;
+    BOOL res = FALSE;
+    if (item->condCode < LOGIC_COND_COUNT)
+        res = condTable[item->condCode](ent, item);
+
+#if ENH_DEBUG_LOGIC
+    const char *name = CondName(item->condCode);
+    Dbg(
+        "CND(0x%08x) %-7s 0x%02x, 0x%02x -> %c\n",
+        (DWORD)ent,
+        name,
+        item->condArg1,
+        item->condArg2,
+        res ? '1' : '0'
+    );
+#endif
+
+    return res;
 }
 
 BOOL CondTrue(void *ent, const LOGIC_ITEM *item)
@@ -254,7 +398,18 @@ BOOL CondTkdesc(void *ent, const LOGIC_ITEM *item)
 
 void RunLogicAction(void *ent, const LOGIC_ITEM *item)
 {
-    if (item->actionCode < ARRAYSIZE(actionTable))
+#if ENH_DEBUG_LOGIC
+    const char *name = ActionName(item->actionCode);
+    Dbg(
+        "ACT(0x%08x) %-7s 0x%04x, 0x%04x\n",
+        (DWORD)ent,
+        name,
+        item->actionArg1,
+        item->actionArg2
+    );
+#endif
+
+    if (item->actionCode < LOGIC_ACTION_COUNT)
         actionTable[item->actionCode](ent, item);
 }
 
